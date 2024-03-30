4e4e0a91a2c28623413bdb28920d617a84d7fd95
Insomiac-Owl <omarkalas2002@gmail.com>
2024-03-30 21:47:00 +0300

    altered a line in the to html function

--- js/buy-now.js
+++ js/buy-now.js
@@ -1,20 +1,20 @@
-const buyerDetailsForm = document.querySelector('#buyerDetailsForm');
-const addOrder = document.querySelector('#addOrder');
+const buyerDetailsForm = document.querySelector("#buyerDetailsForm");
+const addOrder = document.querySelector("#addOrder");
 
-document.addEventListener('DOMContentLoaded', showBuyerDetailsForm);
+document.addEventListener("DOMContentLoaded", showBuyerDetailsForm);
 
 let users = [];
 let items = [];
 
-buyerDetailsForm.addEventListener('submit', async (event) => {
+buyerDetailsForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const purchaseDetails = {   
+  const purchaseDetails = {
     orderNo: Math.floor(Math.random() * 1000000),
-    itemId: parseInt(localStorage.getItem('itemID')),
-    shippingAddress: buyerDetailsForm.querySelector('#shippingAddress').value,
-    quantity: parseInt(buyerDetailsForm.querySelector('#quantity').value),
+    itemId: parseInt(localStorage.getItem("itemID")),
+    shippingAddress: buyerDetailsForm.querySelector("#shippingAddress").value,
+    quantity: parseInt(buyerDetailsForm.querySelector("#quantity").value),
     date: new Date(Date.now()).toISOString(),
-    shippingType: buyerDetailsForm.querySelector('#shippingType').value
+    shippingType: buyerDetailsForm.querySelector("#shippingType").value,
   };
 
   if (!localStorage.users) {
@@ -32,8 +32,12 @@ buyerDetailsForm.addEventListener('submit', async (event) => {
   }
 
   // debugger;
-  const item = items.find(item => item.id === parseInt(localStorage.getItem('itemID')));
-  const user = users.find(user => user.uid === parseInt(localStorage.getItem('loggedInUser')));
+  const item = items.find(
+    (item) => item.id === parseInt(localStorage.getItem("itemID"))
+  );
+  const user = users.find(
+    (user) => user.uid === parseInt(localStorage.getItem("loggedInUser"))
+  );
   const itemPrice = parseInt(item.price);
   const itemQuantity = parseInt(purchaseDetails.quantity);
   if (itemPrice * itemQuantity > user.balance) {
@@ -43,14 +47,32 @@ buyerDetailsForm.addEventListener('submit', async (event) => {
       alert("Insufficient stock");
     } else {
       item.quantity -= itemQuantity;
-      items.splice(items.findIndex(item => item.id === parseInt(localStorage.getItem('itemID'))), 1, item);
+      items.splice(
+        items.findIndex(
+          (item) => item.id === parseInt(localStorage.getItem("itemID"))
+        ),
+        1,
+        item
+      );
       localStorage.items = JSON.stringify(items);
-      const seller = users.find(user => user.itemsOwned && user.itemsOwned.includes(item.id));
+      const seller = users.find(
+        (user) => user.itemsOwned && user.itemsOwned.includes(item.id)
+      );
       seller.bankAccount.balance += itemPrice * itemQuantity;
-      users.splice(users.findIndex(user => user.uid === seller.uid), 1, seller);
+      users.splice(
+        users.findIndex((user) => user.uid === seller.uid),
+        1,
+        seller
+      );
       user.balance -= itemPrice * itemQuantity;
       user.itemsPurchased.push(purchaseDetails);
-      users.splice(users.findIndex(user => user.uid === parseInt(localStorage.getItem('loggedInUser'))), 1, user);
+      users.splice(
+        users.findIndex(
+          (user) => user.uid === parseInt(localStorage.getItem("loggedInUser"))
+        ),
+        1,
+        user
+      );
       localStorage.users = JSON.stringify(users);
       console.log(items);
       console.log(users);
@@ -64,7 +86,7 @@ buyerDetailsForm.addEventListener('submit', async (event) => {
 async function showBuyerDetailsForm() {
   const itemsData = await fetch("js/data/items.json");
   items = await itemsData.json();
-  const item = items.find(item => item.id == localStorage.getItem('itemID'));
+  const item = items.find((item) => item.id == localStorage.getItem("itemID"));
   buyerDetailsForm.innerHTML = buyersDetailsFormToHTML(item);
 }
 
@@ -81,6 +103,6 @@ function buyersDetailsFormToHTML(item) {
             <option value="express">Express</option>
             <option value="priorityMail">Priority Mail</option>
           </select>
-          <label>Total Price: $${item.price}</label>
+          <label> Price of each: $${item.price}</label>
           <button type="submit" id="addOrder">Place Order</button>`;
 }
